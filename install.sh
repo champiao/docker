ee30bbd80b3d04b0b9f32b3452455a957a02dca2
champiao <rafael.leite.assis63@gmail.com>
2020-11-11 09:22:17 -0300

    commit docker installation file

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+import os
+from tqdm import *
+print('aguarde a instalação')
+for i in tqdm(range(100000)):
+	pass
+os.system('curl https://get.docker.com | bash')
+print('concluído')
+exit()
