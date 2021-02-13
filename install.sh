7e2c9af1a138ab54a40ab53728bab3eace2a7ddf
champiao <rafael.leite.assis63@gmail.com>
2021-02-13 11:01:39 -0300

    .

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ~/docker
-sudo apt install pyhton2 python3 curl
-sudo dnf install python2 python3 curl
-sudo yum install python2 python3 curl
-sudo pacman -Syyuu python2 python3 curl
+sudo apt install pyhton2 python3 curl -y
+sudo dnf install python2 python3 curl -y 
+sudo yum install python2 python3 curl -y 
+sudo pacman -Syyuu python2 python3 curl --nonconfirm
 chmod +x ./docker.sh
 ./docker.sh
