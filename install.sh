5dec7494f8cb79b6c9aed67b81f9a4831e7d89f1
champiao <39089594+champiao@users.noreply.github.com>
2021-06-02 11:52:04 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ~/docker
-sudo apt install pyhton2 python3 curl -y
-sudo dnf install python2 python3 curl -y 
-sudo yum install python2 python3 curl -y 
+sudo apt install python3-pip python3 curl -y
+sudo dnf install python3-pip python3 curl -y 
+sudo yum install python3-pip python3 curl -y 
 sudo pacman -Syyuu python2 python3 curl --nonconfirm
+pip3 install --upgrade pip --user
+pip3 install tqdm --user
 chmod +x ./docker.sh
 ./docker.sh
