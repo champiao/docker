0837a987eb09fa5a28fcfa2ccc23eda061df8d6a
champiao <39089594+champiao@users.noreply.github.com>
2021-06-02 11:54:53 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,7 +6,7 @@ import time
 print('aguarde a instalação')
 os.system('curl https://get.docker.com | bash')
 print('installing compose')
-for i in tqdm(range(10000)):
+for i in tqdm(range(100)):
   time.sleep(0.02)
   pass
 os.system('sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose')
