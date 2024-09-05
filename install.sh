42f387d2cefa9e26d0c6eb01fe79fe90a6cdb87a
Ubuntu <ubuntu@ip-172-31-90-116.ec2.internal>
2024-09-05 15:54:24 +0000

    change tqdm installation

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd ~/docker
-sudo apt install python3-pip python3 curl -y
+sudo apt install python3-pip python3 curl python3-tqdm -y
 sudo dnf install python3-pip python3 curl -y 
 sudo yum install python3-pip python3 curl -y 
 sudo pacman -Syyuu python2 python3 curl --nonconfirm
 pip3 install --upgrade pip --user
-pip3 install tqdm --user
 chmod +x ./docker.sh
 ./docker.sh
